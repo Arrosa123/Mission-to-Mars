ac1623227bb644acd425e24b938f99439b842bc1
Your Name <you@example.com>
2022-03-12 19:25:23 -0800

    Add Challenge code

--- Scraping.py
+++ Scraping.py
@@ -8,9 +8,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape_all():
     # Initiate headless driver for deployment
-    executable_path = {'executable_path': ChromeDriverManager().install()}
+     #executable_path = {'executable_path': ChromeDriverManager().install()}
+    executable_path = {'executable_path': 'chromedriver.exe'} # For Windows it is chromedriver.exe
     browser = Browser('chrome', **executable_path, headless=True)
-
+    
+    hemisphere_image_urls = hemisphere(browser)
     news_title, news_paragraph = mars_news(browser)
 
     # Run all scraping functions and store results in a dictionary
@@ -19,6 +21,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": hemisphere_image_urls,
         "last_modified": dt.datetime.now()
     }
 
@@ -31,7 +34,7 @@ def mars_news(browser):
 
     # Scrape Mars News
     # Visit the mars nasa news site
-    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    url = 'https://redplanetscience.com'
     browser.visit(url)
 
     # Optional delay for loading the page
@@ -53,11 +56,13 @@ def mars_news(browser):
         return None, None
 
     return news_title, news_p
-
-
+##END OF RETURNING NEWS TITLE & PARAGRAPH
+ 
+###JPL Space Images Featyred Image####
+##DISPLAYING FEATURED IMAGE
 def featured_image(browser):
     # Visit URL
-    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
     # Find and click the full image button
@@ -67,40 +72,67 @@ def featured_image(browser):
     # Parse the resulting html with soup
     html = browser.html
     img_soup = soup(html, 'html.parser')
+    #img_soup will print fancybox-image html description
+
 
     # Add try/except for error handling
     try:
         # Find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-
+        #img_url_rel will print 'image/featured/mars3.jpg'
     except AttributeError:
         return None
 
     # Use the base url to create an absolute url
-    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
-
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    #img_url will print 'https://spaceimages-mars.com/image/featured/mars3.jpg'
     return img_url
+#END OF RETURNINNG ONE FEATAURED IMAGE
 
+##MARS FACTS
+#TABLES 1 & 2
 def mars_facts():
     # Add try/except for error handling
     try:
         # Use 'read_html' to scrape the facts table into a dataframe
-        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
-
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        # dr.head will print table from [0,2] rows 0 - 4
     except BaseException:
         return None
 
     # Assign columns and set index of dataframe
     df.columns=['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
+    #df will print table with formatted descriptions
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html()
+    ##End of tables
 
+## HEMISPHERES 
+def hemisphere(browser):
+# 1. Use browser to visit the URL 
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)    
+
+# 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    #imgs_links= browser.find_by_css("a.product-item h3")
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(4):      
+    #create empty dictionary
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.find_link_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()    
+    return hemisphere_image_urls
+##END of SCRAPE ALL
 if __name__ == "__main__":
-
     # If running as script, print scraped data
     print(scrape_all())
-
-
-
